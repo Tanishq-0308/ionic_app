d0699c9256c373c8c4fc3bdd22aaa2ebf52cff2c
Tanishq <sharmatanishq0308@gmail.com>
2024-11-26 19:26:28 +0530

    home updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,114 @@
-import { IonButton, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonAvatar, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonChip, IonContent, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonMenuButton, IonPage, IonRefresher, IonRefresherContent, IonSearchbar, IonSkeletonText, IonTitle, IonToolbar, useIonAlert, useIonToast, useIonViewWillEnter } from '@ionic/react';
+import { trashBinOutline } from 'ionicons/icons';
+import React, { useState } from 'react';
 
 const Home: React.FC = () => {
+  const [loading,setLoading]=useState(true);
+  const [users,setUsers]= useState([]);
+  const [showAlert]= useIonAlert();
+  const [showToast]= useIonToast();
+
+  useIonViewWillEnter( async () =>{
+    const users= await getUsers();
+    setUsers(users);
+    setLoading(false);
+  })
+
+  const getUsers=async()=>{
+    const data= await fetch('https://randomuser.me/api?results=10');
+    const users= await data.json();
+    return users.results;
+  }
+
+  const clearList=()=>{
+    showAlert({
+      header:'Confirm!',
+      message:'Are you sure you want to delete all users?',
+      buttons:[
+        {text:'Cancel', role:'cancel'},
+        {
+          text:'Delete',
+          handler:()=>{
+            setUsers([]);
+            showToast({
+              message:'All users deleted',
+              duration:2000,
+              color:'danger',
+            })
+          }
+        }
+      ]
+    })
+  }
+
+  const doRefresh= async (ev:any)=>{
+    const data = await getUsers();
+    setUsers(data);
+    ev.detail.complete();
+  }
 
     return (
         <IonPage>
         <IonHeader>
           <IonToolbar color='primary'>
-          <IonButton slot='start' fill='clear' color={'light'}>
-                    <IonMenuButton/>
+          <IonButton slot='start' fill='clear'>
+                    <IonMenuButton color={'light'}/>
                 </IonButton>
-            <IonTitle>Home</IonTitle>
+            <IonTitle>List</IonTitle>
+            <IonButtons slot='end'>
+              <IonButton onClick={clearList}>
+                <IonIcon slot='icon-only' icon={trashBinOutline} color='light'/>
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+          <IonToolbar color={'primary'}>
+            <IonSearchbar/>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Blank</IonTitle>
-            </IonToolbar>
-          </IonHeader>
+        <IonContent>
+          <IonRefresher slot='fixed' onIonRefresh={(ev)=>doRefresh(ev)}>
+            <IonRefresherContent/>
+          </IonRefresher>
+
+          {loading && 
+            [...Array(10)].map((_,index)=>(
+              <IonCard key={index}> 
+          <IonCardContent className='ion-no-padding'>
+            <IonItem lines='none'>
+              <IonAvatar slot='start'>
+                <IonSkeletonText/>
+              </IonAvatar>
+              <IonLabel>
+              <IonSkeletonText animated style={{width:'150px'}}/>
+                <p><IonSkeletonText/></p>
+              </IonLabel>
+              <IonChip slot='end'>
+                
+              </IonChip>
+            </IonItem>
+          </IonCardContent>
+          </IonCard>
+            ))
+          }
+
+         {users.map((user,index)=>(
+          <IonCard key={index}> 
+          <IonCardContent className='ion-no-padding'>
+            <IonItem lines='none'>
+              <IonAvatar slot='start'>
+                <IonImg src={user.picture.thumbnail}/>
+              </IonAvatar>
+              <IonLabel>
+                {user.name.first} {user.name.last}
+                <p>{user.email}</p>
+              </IonLabel>
+              <IonChip slot='end'>
+                {user.nat}
+              </IonChip>
+            </IonItem>
+          </IonCardContent>
+          </IonCard>
+         ))}
         </IonContent>
       </IonPage>
     );
